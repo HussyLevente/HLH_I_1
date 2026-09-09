91c58898a55cfaa533e27d4426a5095a6f634137
XF0J95 <XF0J95@ZUGEDU-PCD8-42>
2026-09-09 11:07:38 +0200

    Add project files.

---
+++ HLH_I_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HLH_I_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("'a' oldal hossza: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("'b' oldal hossza: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"Terület: {2*(a+b)}, Kerület: {a*b}");
+        }
+    }
+}
---
+++ HLH_I_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HLH_I_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Budapesti Gazdaságtudományi Egyetem")]
+[assembly: AssemblyProduct("HLH_I_1")]
+[assembly: AssemblyCopyright("Copyright © Budapesti Gazdaságtudományi Egyetem 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("bf8268b1-5ac7-4636-ae2a-6b05ba489cf1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
